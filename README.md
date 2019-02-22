41ac41c4f34ffa11ffead25d4ea2c11b8cb97d0d
william_wp <william_wp@hotmail.com>
2019-02-22 21:09:10 +0800

    更新 readme

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# nextcloud
+# 利用旧的x200笔记本搭建 nextcloud 私有云盘
--- config/nextcloud/config/config.php
+++ config/nextcloud/config/config.php
@@ -18,4 +18,22 @@ $CONFIG = array (
     ),
   ),
   'instanceid' => 'ocz8ziupyojv',
+  'passwordsalt' => 'AhfxYNtqIbFWqkinaUgApwwxDTw0jd',
+  'secret' => '0FMb6WdVySpElyaFoqdoQHRBH1PtYv7v6qEZeH06kyUy0Gof',
+  'trusted_domains' => 
+  array (
+    0 => '192.168.232.110:8080',
+  ),
+  'datadirectory' => '/var/www/html/data',
+  'dbtype' => 'mysql',
+  'version' => '15.0.4.0',
+  'overwrite.cli.url' => 'http://192.168.232.110:8080',
+  'dbname' => 'nextcloud',
+  'dbhost' => 'db:3306',
+  'dbport' => '',
+  'dbtableprefix' => 'oc_',
+  'mysql.utf8mb4' => true,
+  'dbuser' => 'nextcloud',
+  'dbpassword' => 'ouMl9/HdcOUIEa1qQOsRYA==',
+  'installed' => true,
 );
